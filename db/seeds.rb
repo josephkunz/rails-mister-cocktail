3c01f3153dba53721ff7b2b61c416e5bacf265b0
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-14 14:49:44 +0100

    cocktail controller & methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# ingredients
+i1 = Ingredient.create(name: "lemon")
+i2 = Ingredient.create(name: "ice")
+i3 = Ingredient.create(name: "mint leaves")
+i4 = Ingredient.create(name: "Gin")
+i5 = Ingredient.create(name: "Vodka")
+i6 = Ingredient.create(name: "Sugar")
+i7 = Ingredient.create(name: "Syrup")
+i8 = Ingredient.create(name: "Tonic")
+
+
+# cocktails
+c1 = Cocktail.create(name: "Gin Tonic")
+
+
+Dose.create(description: "50 ml", cocktail: c1, ingredient: i4)
