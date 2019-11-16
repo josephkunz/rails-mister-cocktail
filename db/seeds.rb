3112c56d585e780c1a7f4a2bf56a925800b04265
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-16 11:01:23 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding started..."
+
 Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
+
+puts "Destroyed all doses, cocktails and ingredients."
 # ingredients
-i1 = Ingredient.create(name: "lemon")
-i2 = Ingredient.create(name: "ice")
-i3 = Ingredient.create(name: "mint leaves")
-i4 = Ingredient.create(name: "Gin")
-i5 = Ingredient.create(name: "Vodka")
-i6 = Ingredient.create(name: "Sugar")
-i7 = Ingredient.create(name: "Syrup")
-i8 = Ingredient.create(name: "Tonic")
+i1 = Ingredient.create(name: "Gin")
+i2 = Ingredient.create(name: "Vodka")
+i3 = Ingredient.create(name: "Rum")
+i4 = Ingredient.create(name: "Tequila")
+i5 = Ingredient.create(name: "Bourbon")
+i6 = Ingredient.create(name: "Blended Scotch whisky")
+i7 = Ingredient.create(name: "Brandy")
+i8 = Ingredient.create(name: "lemon")
+i9 = Ingredient.create(name: "ice")
+i10 = Ingredient.create(name: "mint leaves")
+i11 = Ingredient.create(name: "Sugar")
+i12 = Ingredient.create(name: "Syrup")
+i13 = Ingredient.create(name: "Tonic")
 # cocktails
 c1 = Cocktail.create(name: "Gin Tonic")
 
 d1 = Dose.create(description: "one large shot of Gin", cocktail: c1, ingredient: i4)
 d2 = Dose.create(description: "add some tonic water", cocktail: c1, ingredient: i8)
 
+puts "Added some stuff..."
+puts "Seeding completed!!!"
