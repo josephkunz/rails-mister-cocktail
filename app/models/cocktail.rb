51dd87aac20a27954e5157b82511e95bf67305a8
Joseph Kunz <joseph.kunz@gmx.de>
2019-12-11 22:24:57 +0100

    adds validation to have only letters in cocktail name

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
   mount_uploader :photo, PhotoUploader
 end
